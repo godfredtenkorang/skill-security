04f4f23ff243a4b5277c65ecab452115126e0488
Godey Tech <91687536+godfredtenkorang@users.noreply.github.com>
2025-01-26 23:21:35 +0000

    changes

--- skill_security/settings.py
+++ skill_security/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['godeyinfo.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
